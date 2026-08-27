14ddeb30ebb6d5480d102830e8ee90c0a2aff308
sebas123312231 <arceantezanasebastianalejandro@gmail.com>
2026-08-27 09:54:46 -0400

    Segundo ciclo

--- src/fizzbuzz.spec.js
+++ src/fizzbuzz.spec.js
@@ -4,5 +4,9 @@ describe("FizzBuzz", () => {
   it("deberia devolver elmismo numero si no sigue una regla", () => {
     expect(fizzbuzz(2)).toEqual(2);
   });
+
+  it("deberia devolver Fizz si es multiplo de 3", () => {
+    expect(fizzbuzz(3)).toEqual("Fizz");
+  });
 });
 
